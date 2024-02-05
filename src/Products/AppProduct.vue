b47ba8157ac2db3cb3b686304b7c8f1f3d8d42bb
Gabriel Jaune Ribeira <gjaune@tcerr.gov.br>
2024-02-05 11:47:06 -0400

    vuex add

--- src/Products/AppProduct.vue
+++ src/Products/AppProduct.vue
@@ -1,13 +1,17 @@
 <template>
   <div>
-    
+    <pre>
+      {{ product }}
+    </pre>
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  props: {
+    product: Object,
+  }
+}  
 </script>
 
 <style>
